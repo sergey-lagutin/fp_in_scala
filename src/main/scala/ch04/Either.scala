87770f826a555832dcd320bed0f3bce9a048ee3b
sl <sl@anahoret.com>
2015-08-06 20:31:28 +0300

    Add Either class.

---
+++ src/main/scala/ch04/Either.scala
@@ -0,0 +1,26 @@
+package ch04
+
+sealed trait Either[+E, +A] {
+  def map[B](f: A => B): Either[E, B] = this match {
+    case Right(a) => Right(f(a))
+    case Left(e) => Left(e)
+  }
+
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Right(a) => f(a)
+    case Left(e) => Left(e)
+  }
+
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case Right(a) => Right(a)
+    case _ => b
+  }
+
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    flatMap(ax => b.map(bx => f(ax, bx)))
+
+}
+
+case class Left[+E](e: E) extends Either[E, Nothing]
+
+case class Right[+A](a: A) extends Either[Nothing, A]
